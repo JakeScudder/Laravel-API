8137f264e50f2d53a9a431d9188023ec1e11897f
Jake Scudder <scudsterr@gmail.com>
2021-05-04 15:53:28 -0400

    Laravel Skeleton

---
+++ laravel-sanctum-api/routes/api.php
@@ -0,0 +1,31 @@
+<?php
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::get("/products", [ProductController::class, "index"]);
+
+
+// Route::post("/products", function() {
+//     return Product::create([
+//         "name" => "Product One",
+//         "slug" => "product-one",
+//         "description" => "This is product one",
+//         "price" => "99.99"
+//     ]);
+// });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
