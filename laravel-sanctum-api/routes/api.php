3a221c4066cd70f02fe47da5c4ff2824f3724d26
Jake Scudder <scudsterr@gmail.com>
2021-05-06 14:58:45 -0400

    Basic CRUD api with auth using Laravel

--- laravel-sanctum-api/routes/api.php
+++ laravel-sanctum-api/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,17 +15,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+// Route::resource("products", ProductController::class);
+
+// Public Routes
+
 Route::get("/products", [ProductController::class, "index"]);
+Route::get("/products/{id}", [ProductController::class, "show"]);
+Route::get("/products/search/{name}", [ProductController::class, "search"]);
+Route::post("/register", [AuthController::class, "register"]);
+Route::post("/login", [AuthController::class, "login"]);
+
+// Private Routes
+Route::group(["middleware" => ["auth:sanctum"]], function () {
+    Route::post("/products", [ProductController::class, "store"]);
+    Route::put("/products/{id}", [ProductController::class, "update"]);
+    Route::delete("/products/{id}", [ProductController::class, "destroy"]);
+    Route::post("/logout", [AuthController::class, "logout"]);
+    
+});
 
 
-// Route::post("/products", function() {
-//     return Product::create([
-//         "name" => "Product One",
-//         "slug" => "product-one",
-//         "description" => "This is product one",
-//         "price" => "99.99"
-//     ]);
-// });
+
+
+
+
+
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
